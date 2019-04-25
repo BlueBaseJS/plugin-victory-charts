c76a714d2d630961a8ec085239523529afe7dbbb
adnan1naeem <adnan1naeem@gmail.com>
2019-04-25 12:37:52 +0500

    feat(updated feature of victory-charts): updated victory-charts plugin

--- src/components/_test_/index.test.tsx
+++ src/components/_test_/index.test.tsx
@@ -3,14 +3,23 @@ import Plugin from '../../index';
 
 test('Plugin should be correctly registered', async () => {
 
-	require('../../components/index.native');
-	require('../../components/index');
-	require('../../bluerain-plugin-victory-charts');
-
 	const BB = new BlueBase();
 	await BB.Plugins.register(Plugin);
-
 	expect(BB.Plugins.has('plugin-victory-charts')).toBeTruthy();
 });
 
+
+test('Plugin should  return plugin victory charts', async () => {
+	const plugin = require('../../bluerain-plugin-victory-charts');
+	expect(plugin).toBeDefined();
+});
+test('Plugin should  return plugin victory charts', async () => {
+
+	require('../../components/index.native');
+	const plugin = require('../../components/index.ts');
+	const BB = new BlueBase();
+	await BB.Plugins.register(Plugin);
+
+	expect(plugin).toBeDefined();
+});
 
